39120f08d153b8bf65bbd49dbb9e672b401a3337
Dharmesh Singh <63810275+DharmeshSingh01@users.noreply.github.com>
2020-04-28 03:04:01 +0530

    Upload
    Uplaod

---
+++ DatingApp.API/DataContext/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.DataContext
+{
+    public class DataContexts :DbContext
+    {
+        public DataContexts(DbContextOptions<DataContexts> options) :base(options)
+        {
+
+        }
+
+        DbSet<Values> value { get; set; }
+    }
+}
